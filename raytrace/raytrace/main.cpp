61143b1a4925b61bdd84e229cbc90e5943a67fef
Yash Patel <yppatel@stat-yppatel-mba.local>
2021-10-09 10:21:01 -0400

    Init project

---
+++ raytrace/raytrace/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
